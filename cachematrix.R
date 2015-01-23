2970f79a929beda1fe8d2ad6fe7dd5e6323af7ed
AnikoM <anmedgyesi@gmail.com>
2015-01-23 20:41:58 +0100

    R Programming Assignment2
    Function to calculate inverse matrix using function environments to store the already calculated value

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions together able to give the inverse matrix of another
+## If once it already was calculated, the value will be stored in makeCacheMatrix function environment
 
-## Write a short comment describing this function
+## This function a special type of matrix object with functions to store the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  stopifnot(is.matrix(x))
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setInvers<-function(solve) m<<-solve
+  #function to input the invert value (not calculate just store!)
+  getInvers<-function() m
+  list(set=set,get=get,
+       setInvers=setInvers,
+       getInvers=getInvers)
 }
 
 
-## Write a short comment describing this function
+## This function able to calculate the inverse of the output object "matrix" defined in the makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  #Check if there any inverse stored already
+  m<-x$getInvers() 
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+    #function stops, m value returns
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setInvers(m)
+  #value of m stored in the environment of setinvers function
+  m
 }
